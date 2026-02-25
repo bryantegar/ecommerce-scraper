85b2b93c1085e04fc503fcc3afb58ccc7f06154c
Aditya Pradana <dipta.aryadana@gmail.com>
2026-02-25 09:48:20 +0700

    worker crawl keyword lazada

---
+++ workers/base_worker.py
@@ -0,0 +1,44 @@
+import sys
+
+from redis import Redis
+
+from libs.cookies_manager import get_cookies
+from libs.exc import NoAvailableResourceException
+from libs.logger import printerror, printinfo
+from settings import REDIS
+
+
+class BaseWorker:
+    def __init__(self, config='default', allowed_usage='for_auto', use_proxy=True):
+        self.config = config
+        self.allowed_usage = allowed_usage
+        self.use_proxy = use_proxy
+        self.cookies = None
+        self.resource_id = None
+        self.user_credential = None
+        self.complete_cookie = None
+        self.from_file = None
+        self.cookie_filename = None
+
+    def set_resources(self, social_media, filename):
+        printinfo(f'Requesting Cookies - Allowed Usage: {self.allowed_usage}')
+
+        try:
+            cookies, resource_id, user_credential, complete_cookie, from_file = get_cookies(
+                filename, self.allowed_usage, social_media, retries=1)
+        except NoAvailableResourceException as e:
+            printerror('No Cookies Available')
+            sys.exit(1)
+
+        self.cookies = cookies
+        self.resource_id = resource_id
+        self.user_credential = user_credential
+        self.complete_cookie = complete_cookie
+        self.from_file = from_file
+        self.cookie_filename = filename
+        printinfo('Resource ID: ' + self.resource_id)
+
+    def set_conn_redis(self):
+        self.conn_redis = Redis(host=REDIS[self.config]['host'],
+                                port=REDIS[self.config]['port'], decode_responses=True,
+                                password=REDIS[self.config]['password'])
---
+++ workers/worker_lazada.py
@@ -0,0 +1,94 @@
+from itertools import cycle
+import json
+import keyword
+import socket
+from time import sleep
+from weakref import proxy
+
+from sentry_sdk import capture_exception
+
+from libs.beans import Pusher, Worker
+from libs.exc import HTTPStatusException
+from libs.graceful_killer import GracefulKiller
+from libs.logger import printinfo
+from libs.proxies import get_proxy_cycle
+from services.service_general import store_raw
+from services.service_lazada import ServiceLazada
+from settings import BEANS
+from workers.base_worker import BaseWorker
+
+HOSTNAME = socket.gethostname()
+
+
+class WorkerLazada(BaseWorker):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def handle_exception(self, e: Exception, job=None):
+        printinfo(f"Error processing job: {str(e)}")
+        if job:
+            capture_exception(e)
+            self.worker.buryJob(job)
+
+    def worker_scrape_keyword(self):
+        printinfo("----------------------------------")
+        printinfo("Starting Worker Lazada Keyword")
+        tubename = f'{BEANS[self.config]["prefix"]}_crawler_lazada_keyword'
+        worker = Worker(
+            tubename,
+            BEANS[self.config]['host'],
+            BEANS[self.config]['port'])
+        pusher_self = Pusher(
+            tubename,
+            host=BEANS[self.config]['host'],
+            port=BEANS[self.config]['port'])
+        self.worker = worker
+        self.set_conn_redis()
+        self.set_resources('lazada', 'lazada')
+        killer = GracefulKiller()
+        if self.use_proxy:
+            proxy_cycle = get_proxy_cycle()
+            printinfo('Proxy Loaded')
+        else:
+            proxy_cycle = cycle([None])
+
+        service = ServiceLazada()
+
+        while not killer.kill_now:
+            self.current_proxy = next(proxy_cycle)
+            job = worker.getJob()
+            if not job:
+                sleep(10)
+            else:
+                try:
+                    crawl_next = True
+                    message = json.loads(job.body)
+                    keyword = message['content']
+                    count = message['count'] if 'count' in message else 0
+                    max_count = message['max_count'] if 'max_count' in message else 0
+
+                    resp = service.scrape_lazada(
+                        keyword, self.cookies, page=count, proxy=self.current_proxy)
+
+                    if resp.status_code == 200:
+                        fname = store_raw(resp, prefix='lzd-kw', hostname=HOSTNAME,
+                                          keyword=keyword,
+                                          cookie=self.complete_cookie, social_media='lazada')
+                        printinfo('Saved to: '+fname)
+                    else:
+                        raise HTTPStatusException(
+                            resp.status_code,
+                            f"Keyword: {keyword} - page: {count}", resp=resp
+                        )
+                    if count >= max_count:
+                        crawl_next = False
+
+                    worker.deleteJob(job)
+
+                    if not crawl_next:
+                        self.conn_redis.srem(tubename, keyword)
+                    else:
+                        message['count'] = count + 1
+                        pusher_self
+                except Exception as e:
+                    self.handle_exception(e, job)
