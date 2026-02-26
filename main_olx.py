e14ce8c60ba81ab90fb36b247448b52b8f462fbb
Aditya Pradana <dipta.aryadana@gmail.com>
2026-02-26 10:36:20 +0700

    - worker olx keyword

---
+++ main_olx.py
@@ -0,0 +1,49 @@
+from workers.worker_olx import WorkerOLX
+import argparse
+import sentry_sdk
+from settings import SENTRY
+from sentry_sdk import set_tags
+
+
+if __name__ == '__main__':
+    choices = [
+        'worker_olx_keyword',
+    ]
+    parser = argparse.ArgumentParser(description='Worker OLX',
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-m', '--mode', metavar='', type=str,
+                        help='Worker Mode',
+                        choices=choices)
+    parser.add_argument('--max-page', metavar='', type=int,
+                        help='Max page to be crawled',
+                        default=20)
+    parser.add_argument('-c', '--config', metavar='', type=str,
+                        help='Config File', default='default')
+    parser.add_argument('--allowed-usage', metavar='', type=str,
+                        help='Cookie Allowed Usage', default='for_auto')
+    parser.add_argument('--cookie',
+                        help='Use Cookie', action='store_true')
+    parser.add_argument('--do-not-use-proxy',
+                        help='Do not use proxy', action='store_false', default=True)
+    parser.set_defaults(cookie=False)
+    args = parser.parse_args()
+    mode = args.mode
+    allowed_usage = args.allowed_usage
+    config = args.config
+
+    sentry_sdk.init(
+        dsn=SENTRY[config]['dsn'],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        environment=SENTRY[config]['env']
+    )
+    set_tags({'process.social_media': 'olx',
+              'process.name': mode})
+    if mode == 'worker_olx_keyword':
+        worker_olx = WorkerOLX(
+            allowed_usage=args.allowed_usage,
+            use_proxy=args.do_not_use_proxy)
+        worker_olx.worker_scrape_keyword()
---
+++ workers/worker_olx.py
@@ -0,0 +1,86 @@
+from itertools import cycle
+import json
+import socket
+from time import sleep
+
+from sentry_sdk import capture_exception
+
+from libs.beans import Pusher, Worker
+from libs.exc import HTTPStatusException
+from libs.graceful_killer import GracefulKiller
+from libs.logger import printinfo
+from libs.proxies import get_proxy_cycle
+from services.service_general import store_raw
+from services.service_olx import ServiceOLX
+from settings import BEANS
+from workers.base_worker import BaseWorker
+
+HOSTNAME = socket.gethostname()
+
+
+class WorkerOLX(BaseWorker):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def handle_exception(self, e: Exception, job=None):
+        printinfo(f"Error processing job: {str(e)}")
+        if job:
+            capture_exception(e)
+            self.worker.buryJob(job)
+
+    def worker_scrape_keyword(self):
+        printinfo("----------------------------------")
+        printinfo("Starting Worker OLX Keyword")
+        tubename = f'{BEANS[self.config]["prefix"]}_crawler_olx_keyword'
+        worker = Worker(
+            tubename,
+            BEANS[self.config]['host'],
+            BEANS[self.config]['port'])
+        pusher_self = Pusher(
+            tubename,
+            host=BEANS[self.config]['host'],
+            port=BEANS[self.config]['port'])
+        self.worker = worker
+        self.set_conn_redis()
+        self.set_resources('olx', 'olx')
+        killer = GracefulKiller()
+        if self.use_proxy:
+            proxy_cycle = get_proxy_cycle()
+            printinfo('Proxy Loaded')
+        else:
+            proxy_cycle = cycle([None])
+
+        service = ServiceOLX()
+
+        while not killer.kill_now:
+            self.current_proxy = next(proxy_cycle)
+            job = worker.getJob()
+            if not job:
+                sleep(10)
+            else:
+                try:
+                    crawl_next = True
+                    message = json.loads(job.body)
+                    keyword = message['content']
+                    count = message['count'] if 'count' in message else 0
+                    max_count = message['max_count'] if 'max_count' in message else 0
+                    resp = service.scrape_olx(
+                        keyword,  page=count, proxy=self.current_proxy)
+                    if resp.status_code == 200:
+                        fname = store_raw(resp, prefix='olx-kw', hostname=HOSTNAME,
+                                          keyword=keyword, page=count)
+                        printinfo('Saved to: '+fname)
+                    else:
+                        raise HTTPStatusException(
+                            resp.status_code,
+                            f"Keyword: {keyword} - page: {count}",
+                            resp=resp
+                        )
+                    if count >= max_count:
+                        crawl_next = False
+                    if crawl_next:
+                        message['count'] = count + 1
+                        pusher_self.setJob(json.dumps(message))
+                except Exception as e:
+                    self.handle_exception(e, job)
+        self.worker_exit()
